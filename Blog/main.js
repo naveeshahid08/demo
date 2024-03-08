82393411949c9e292fff06e6b42e0408c6fa36f4
Naveed Shahid <naveedshahid08@gmail.com>
2024-03-08 14:08:52 +0200

    Week8 JS

---
+++ Blog/main.js
@@ -0,0 +1,28 @@
+/* const fetchData = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((json) => displayData(json));
+}; */
+
+const fetchData = async () => {
+  try {
+    const reponse = await fetch("https://jsonplaceholder.typicode.com/posts");
+    if (!reponse.ok) {
+      throw new Error(`Error status:${reponse.status}`);
+    }
+
+    const data = await reponse.json();
+    displayData(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+fetchData();
+const displayData = (data) => {
+  const container = document.querySelector("#blogContainer");
+  data.forEach((post) => {
+    const postElement = document.createElement("div");
+    postElement.innerHTML = `<h2>${post.title}</h2> <p>${post.body}</p>`;
+    container.appendChild(postElement);
+  });
+};
